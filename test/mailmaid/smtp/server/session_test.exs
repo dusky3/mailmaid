21b1ae0bc04e99424135f48b1e6286b6c355cfc5
Corey Powell <mistdragon100@gmail.com>
2017-04-04 12:51:59 -0500

    Added bare newline tests

--- test/mailmaid/smtp/server/session_test.exs
+++ test/mailmaid/smtp/server/session_test.exs
@@ -78,4 +78,49 @@ defmodule Mailmaid.SMTP.Server.SessionTest do
       assert {<<>>, <<>>} == Session.parse_request(<<"">>)
     end
   end
+
+  describe "stray newline test" do
+
+    test "Error out by default" do
+      assert <<"foo">> == Session.check_bare_crlf(<<"foo">>, <<>>, false, 0)
+      assert :error == Session.check_bare_crlf(<<"foo\n">>, <<>>, false, 0)
+      assert :error == Session.check_bare_crlf(<<"fo\ro\n">>, <<>>, false, 0)
+      assert :error == Session.check_bare_crlf(<<"fo\ro\n\r">>, <<>>, false, 0)
+      assert <<"foo\r\n">> == Session.check_bare_crlf(<<"foo\r\n">>, <<>>, false, 0)
+      assert <<"foo\r">> == Session.check_bare_crlf(<<"foo\r">>, <<>>, false, 0)
+    end
+
+    test "Fixing them should work" do
+      assert <<"foo">> == Session.check_bare_crlf(<<"foo">>, <<>>, :fix, 0)
+      assert <<"foo\r\n">> == Session.check_bare_crlf(<<"foo\n">>, <<>>, :fix, 0)
+      assert <<"fo\r\no\r\n">> == Session.check_bare_crlf(<<"fo\ro\n">>, <<>>, :fix, 0)
+      assert <<"fo\r\no\r\n\r">> == Session.check_bare_crlf(<<"fo\ro\n\r">>, <<>>, :fix, 0)
+      assert <<"foo\r\n">> == Session.check_bare_crlf(<<"foo\r\n">>, <<>>, :fix, 0)
+    end
+
+    test "Stripping them should work" do
+      assert <<"foo">> == Session.check_bare_crlf(<<"foo">>, <<>>, :strip, 0)
+      assert <<"foo">> == Session.check_bare_crlf(<<"fo\ro\n">>, <<>>, :strip, 0)
+      assert <<"foo\r">> == Session.check_bare_crlf(<<"fo\ro\n\r">>, <<>>, :strip, 0)
+      assert <<"foo\r\n">> == Session.check_bare_crlf(<<"foo\r\n">>, <<>>, :strip, 0)
+    end
+
+    test "Ignoring them should work" do
+      assert <<"foo">> == Session.check_bare_crlf(<<"foo">>, <<>>, :ignore, 0)
+      assert <<"fo\ro\n">> == Session.check_bare_crlf(<<"fo\ro\n">>, <<>>, :ignore, 0)
+      assert <<"fo\ro\n\r">> == Session.check_bare_crlf(<<"fo\ro\n\r">>, <<>>, :ignore, 0)
+      assert <<"foo\r\n">> == Session.check_bare_crlf(<<"foo\r\n">>, <<>>, :ignore, 0)
+    end
+
+    test "Leading bare LFs should check the previous line" do
+      assert <<"\nfoo\r\n">> == Session.check_bare_crlf(<<"\nfoo\r\n">>, <<"bar\r">>, false, 0)
+      assert <<"\r\nfoo\r\n">> == Session.check_bare_crlf(<<"\nfoo\r\n">>, <<"bar\r\n">>, :fix, 0)
+      assert <<"\nfoo\r\n">> == Session.check_bare_crlf(<<"\nfoo\r\n">>, <<"bar\r">>, :fix, 0)
+      assert <<"foo\r\n">> == Session.check_bare_crlf(<<"\nfoo\r\n">>, <<"bar\r\n">>, :strip, 0)
+      assert <<"\nfoo\r\n">> == Session.check_bare_crlf(<<"\nfoo\r\n">>, <<"bar\r">>, :strip, 0)
+      assert <<"\nfoo\r\n">> == Session.check_bare_crlf(<<"\nfoo\r\n">>, <<"bar\r\n">>, :ignore, 0)
+      assert :error == Session.check_bare_crlf(<<"\nfoo\r\n">>, <<"bar\r\n">>, false, 0)
+      assert <<"\nfoo\r\n">> == Session.check_bare_crlf(<<"\nfoo\r\n">>, <<"bar\r">>, false, 0)
+    end
+  end
 end
